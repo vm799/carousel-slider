5a6a6e39a7c037ebeea2cf86c2637bb83a434d9a
vm799 <89162649+vm799@users.noreply.github.com>
2022-01-01 21:01:24 +0000

    js functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dots = document.querySelector(".dotsContainer")
 const prev = document.querySelector(".prevBtn").addEventListener("click", showPrevImg)
 const next = document.querySelector(".nextBtn").addEventListener("click", showNextImg)
 
-const slideIndex = 0;
+let imgNum = 0;
 
 const imgs = [
     {src :"https://images.unsplash.com/photo-1596574620648-98e00b6a09e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", alt:"viper image"},
@@ -14,21 +14,32 @@ const imgs = [
    {src:"https://images.unsplash.com/photo-1571391733814-15ac29ac3544?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80", alt:"pink viper"}
 ]
 
+function renderImgs(){
     imgs.map(image => {
         const imgSlide = document.createElement("img")
         imgSlide.src = image.src
+        imgSlide.alt = image.alt
         const imgDot = document.createElement("div")
         imgDot.classList.add("dot")
-        slides.appendChild("imgSlide")
-        dotsContainer.appendChild("imgDot")
+        slides.appendChild(imgSlide)
+        dotsContainer.appendChild(imgDot)
         })
-
-
-
-function showPrevImg(){
-    console.log("boyysasasaa")
 }
+    
+
+
 
 function showNextImg(){
-    console.log("boybooyakashaa")
+if (imgNum < imgs.length - 1){
+imgNum += 1
+} else if(imgNum === imgs.length){
+    imgNum = 0
+}
+   
+}
+
+function showPrevImg(){
+   
+    
+    
 }
