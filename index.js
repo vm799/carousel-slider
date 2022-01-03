984180755d26b36f9212964817778f4bcee1e7c3
vm799 <89162649+vm799@users.noreply.github.com>
2022-01-03 09:38:21 +0000

    button styling

--- index.js
+++ index.js
@@ -6,27 +6,31 @@ const next = document.querySelector(".nextBtn").addEventListener("click", showNe
 
 let imgNum = 0;
 
+
 const imgs = [
     {src :"https://images.unsplash.com/photo-1596574620648-98e00b6a09e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", alt:"viper image"},
    
    {src:"https://images.unsplash.com/photo-1570741066052-817c6de995c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80", alt:"pink viper"},
 
-   {src:"https://images.unsplash.com/photo-1571391733814-15ac29ac3544?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80", alt:"pink viper"}
+   {src:"https://images.unsplash.com/photo-1571391733814-15ac29ac3544?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80", alt:"red viper"}
 ]
 
 function renderImgs(){
     imgs.map(image => {
+        console.log("im rnederdrin")
         const imgSlide = document.createElement("img")
         imgSlide.src = image.src
         imgSlide.alt = image.alt
         const imgDot = document.createElement("div")
         imgDot.classList.add("dot")
         slides.appendChild(imgSlide)
-        dotsContainer.appendChild(imgDot)
+        dots.appendChild(imgDot)
         })
 }
+  
+function hideAllSlides(){
     
-
+}
 
 
 function showNextImg(){
@@ -35,11 +39,19 @@ imgNum += 1
 } else if(imgNum === imgs.length){
     imgNum = 0
 }
-   
+
+renderImgs()  
 }
 
 function showPrevImg(){
-   
-    
-    
+    if (imgNum === 0){
+        imgNum = imgs.length - 1
+        } else {
+            imgNum -= 1
+        }
+        
+        renderImgs()   
 }
+
+
+
